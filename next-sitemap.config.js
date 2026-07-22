a08b9854f42a9a051e440716fc8435558c413628
Toufiquer <toufiquerabc@gmail.com>
2026-07-23 05:42:50 +0600

    done @ update SEO content

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl:
+    process.env.SITE_URL ||
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    process.env.NEXT_PUBLIC_BASE_URL ||
+    'https://wesassociates.com',
+  generateRobotsTxt: true,
+};
